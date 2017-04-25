ea1ee7e809ce92e683505ee75f835a9534c3aa64
ljpatriz <ljpatriz@gmail.com>
2017-04-25 13:51:53 -0400

    Got rid of some TODO's

--- src/View/View.java
+++ src/View/View.java
@@ -14,11 +14,10 @@ import java.util.Scanner;
 
 //// TODO: 4/25/2017 fix resizing of window so that the text area resizes and console doesnt
 //// TODO: 4/25/2017 allow user resizing of components
-//// TODO: 4/25/2017 stop using silly fonts this is a professional application
 //// TODO: 4/25/2017 use fxml for layout stuff
 //// TODO: 4/25/2017 have option for user to view registers in base 10
 //// TODO: 4/25/2017 consider a (still bland but slightly) less bland color scheme
-//// TODO: 4/25/2017 menu bar -> file -> save, open, quit
+//// TODO: 4/25/2017 menu bar -> file -> save, open, quit (Larry - done, but need to fix interactions)
 //// TODO: 4/25/2017 finish the application instead of writing so many todos
 
 public class View {
