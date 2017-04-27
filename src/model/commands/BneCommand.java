31ebb04978b113fd94b8a3dfb33effab3b726739
JacobAdamson <jadamson@colby.edu>
2017-04-27 13:43:02 -0400

    Refactored BneCommand

--- src/model/commands/BneCommand.java
+++ src/model/commands/BneCommand.java
@@ -1,21 +1,24 @@
 package model.commands;
 
 import model.Core;
-
+import model.Register;
 /**
  * Created by ncameron on 4/27/2017.
  */
 public class BneCommand extends Command {
-    public BneCommand(String arguments, Core core) {
+    Register src1;
+    Register src2;
+    int targetLoc;
+    public BneCommand(String arguments, Core core) {//TODO make the
         super(arguments, core);
+        arguments.replaceAll(", "," ");
+        String[] split = arguments.split(" ");
+        src1 = core.getRegister(split[0]);
+        src2 = core.getRegister(split[1]);
+        targetLoc = Integer.valueOf(split[2]);
         this.function = s -> {
-            s.replaceAll(", "," ");
-            String[] split = s.split(" ");
-            String src1 = split[0];
-            String src2 = split[1];
-            String target = split[2];
-            if(core.getRegister(src1).getValue() != core.getRegister(src2).getValue())
-                core.setCommandLocation(Integer.valueOf(target));
+            if(src1.getValue() != src2.getValue())
+                core.setCommandLocation(targetLoc);
         };
     }
 }
