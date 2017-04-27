1de249d3363138d7be248ab5fd026ffcbbd4438e
JacobAdamson <jadamson@colby.edu>
2017-04-27 11:04:46 -0400

    OriCommand fix

--- src/model/commands/OriCommand.java
+++ src/model/commands/OriCommand.java
@@ -8,5 +8,14 @@ import model.Core;
 public class OriCommand extends Command {
     public OriCommand(String arguments, Core core) {
         super(arguments, core);
+        this.function = s -> {
+            s.replaceAll(", "," ");
+            String[] split = s.split(" ");
+            String dest = split[0];
+            String src = split[1];
+            String immediate = split[2];
+            int val = Integer.valueOf(immediate);
+            core.getRegister(dest).setValue((core.getRegister(src).getValue()|val));
+        };
     }
 }
