6574e28db086224837a5277477f50b5fd52ec2b1
nickcameron <ncameron@colby.edu>
2017-04-28 21:49:53 -0400

    or multiply

--- src/model/commands/MultCommand.java
+++ src/model/commands/MultCommand.java
@@ -11,6 +11,36 @@ public class MultCommand extends Command {
     Register src;
     Register src2;
     //TODO differentiate between Mul nd Mult
+    //// TODO: 4/28/2017 it's in the mips help manual and in the github page
+    //// TODO: 4/28/2017 more disappointment
+    /*
+                                 __
+                       _ ,___,-'",-=-.
+           __,-- _ _,-'_)_  (""`'-._\ `.
+        _,'  __ |,' ,-' __)  ,-     /. |
+      ,'_,--'   |     -'  _)/         `\
+    ,','      ,'       ,-'_,`           :
+    ,'     ,-'       ,(,-(              :
+         ,'       ,-' ,    _            ;
+        /        ,-._/`---'            /
+       /        (____)(----. )       ,'
+      /         (      `.__,     /\ /,
+     :           ;-.___         /__\/|
+     |         ,'      `--.      -,\ |
+     :        /            \    .__/
+      \      (__            \    |_
+       \       ,`-, *       /   _|,\
+        \    ,'   `-.     ,'_,-'    \
+       (_\,-'    ,'\")--,'-'       __\
+        \       /  // ,'|      ,--'  `-.
+         `-.    `-/ \'  |   _,'         `.
+            `-._ /      `--'/             \
+               ,'           |              \
+              /             |               \
+           ,-'              |               /
+          /                 |             -'
+     */
+
     public MultCommand(Core core, Register dest, Register src, Register src2) {
         super(core);
         this.dest = dest;
