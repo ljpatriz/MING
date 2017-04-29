109cea8deeda0c2560525825469cd99381075f34
nickcameron <ncameron@colby.edu>
2017-04-28 21:29:53 -0400

    Jacob Adamson does not know how to divide

--- src/model/commands/DivCommand.java
+++ src/model/commands/DivCommand.java
@@ -7,6 +7,13 @@ import model.Register;
  * Created by ncameron on 4/27/2017.
  */
 public class DivCommand extends Command {
+    //// TODO: 4/28/2017 jake i'm disappointed
+    //// TODO: 4/28/2017 i told you to look at the github or the MARS help dialog
+    //// TODO: 4/28/2017 leaving this as it is for now so you can feel ashamed
+    //// TODO: 4/28/2017 this is not how div works in mips
+    //// TODO: 4/28/2017 it doesn't take 3 registers, and it interacts with the HI and LO registers
+    //// TODO: 4/28/2017 which we have yet to create
+
     Register dest;
     Register src;
     Register src2;
