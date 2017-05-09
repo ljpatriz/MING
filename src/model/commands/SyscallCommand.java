86abcb579a4a72177174459ab39499d9559e74bd
JacobAdamson <jadamson@colby.edu>
2017-05-08 21:44:15 -0400

    HAHAHAHAHA LARRY IS WRONG

--- src/model/commands/SyscallCommand.java
+++ src/model/commands/SyscallCommand.java
@@ -7,14 +7,16 @@ import model.Register;
  * Created by ncameron on 4/27/2017.
  */
 public class SyscallCommand extends Command {
-    int v0RegisterValue;
     public SyscallCommand(Core core) {
         super(core);
-        v0RegisterValue = core.getRegister("$v0").getValue();
+    }
+    public void apply(){
+        int v0RegisterValue = core.getRegister("$v0").getValue();
         switch(v0RegisterValue){
             case 1:
                 //TODO: Larry - eventually connect this to the UI
                 System.out.println(core.getRegister("$a0").getValue());
+
             case 5:
                 //TODO: Larry - eventually connect this to the UI
                 System.out.println("Enter an integer: ");
@@ -23,7 +25,5 @@ public class SyscallCommand extends Command {
             case 10:
                 //sets the ProgramCounter to the last command (which is a no op command)
                 core.setPC(core.getCommandList().size()-1);
-        }
-
-    }
+        }}
 }
